b30d2b2403943dcd13f3c71e2ac34626842ecf90
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-27 11:59:30 -0700

    Create function to select multiple checkboxes with shift in script.js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const checkboxes = document.querySelectorAll('input');
+
+let lastChecked;
+
+function handleCheck(e) {
+    
+    // Check if shift key is pressed and the box is unchecked
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        // loop over every checkbox
+        checkboxes.forEach(checkbox => {
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log('Inbetween');
+            }
+
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+            console.log(checkbox);
+        });
+    }
+
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
